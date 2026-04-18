608aa79fed7745ea287763a01bc498190e90880d
Nguyen, James <james.nguyen2+origin@originenergy.com.au>
2026-04-18 23:45:53 +1000

    feat: add new render function and input handler

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use color_eyre::eyre::Result;
-use ratatui::DefaultTerminal;
+use ratatui::{
+    DefaultTerminal, Frame,
+    crossterm::event::{self, Event},
+    widgets::{Paragraph, Widget},
+};
 
 fn main() -> Result<()> {
     // Install color_eyre for enhanced error reporting.
@@ -17,5 +21,25 @@ fn main() -> Result<()> {
 }
 
 fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        // Rendering
+        terminal.draw(render)?;
+        // Input handling
+        if let Event::Key(key) = event::read()? {
+            // Handle key events here. For example, you could check for specific keys to exit the loop.
+            match key.code {
+                // Exit the app
+                event::KeyCode::Esc => {
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+
     Ok(())
+}
+
+fn render(frame: &mut Frame) {
+    Paragraph::new("Hello, Ratatui!").render(frame.area(), frame.buffer_mut());
 }
