6159cc28abfc912b0ecba0a49bc0b7e7b20bee6e
npnquang <nguyenjoe0201@gmail.com>
2026-04-18 19:05:52 +1000

    feat: set up a run loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::Result;
+use ratatui::DefaultTerminal;
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let terminal = ratatui::init();
+    let result = run(terminal);
+
+    ratatui::restore();
+    result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    Ok(())
 }
