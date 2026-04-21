329c78c05ea055f7f64450148126821e830ff7a7
Nguyen, James <james.nguyen2+origin@originenergy.com.au>
2026-04-21 22:59:50 +1000

    feat: make the application render the list of items

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use color_eyre::eyre::Result;
 use ratatui::{
     DefaultTerminal, Frame,
     crossterm::event::{self, Event},
-    widgets::{Paragraph, Widget},
+    layout::{Constraint, Layout},
+    style::{Color, Stylize},
+    widgets::{Block, BorderType, List, ListItem, Widget},
 };
 
 #[derive(Debug, Default)]
 struct AppState {
-    todo_items: Vec<TodoItem>,
+    items: Vec<TodoItem>,
 }
 
 #[derive(Debug, Default)]
@@ -18,23 +20,28 @@ struct TodoItem {
 
 fn main() -> Result<()> {
     // Install color_eyre for enhanced error reporting.
+    let mut state = AppState::default();
+    state.items.push(TodoItem {
+        is_done: false,
+        description: "Buy groceries".to_string(),
+    });
     color_eyre::install()?;
 
     // Initialize the terminal using ratatui. This will set up the terminal for drawing.
     let terminal = ratatui::init();
 
     // Run the main application logic, passing the initialized terminal. The result will be returned to the caller.
-    let result = run(terminal);
+    let result = run(terminal, &mut state);
 
     // Restore the terminal state before exiting, even if an error occurs.
     ratatui::restore();
     result
 }
 
-fn run(mut terminal: DefaultTerminal) -> Result<()> {
+fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
     loop {
         // Rendering
-        terminal.draw(render)?;
+        terminal.draw(|frame| render(frame, app_state))?;
         // Input handling
         if let Event::Key(key) = event::read()? {
             // Handle key events here. For example, you could check for specific keys to exit the loop.
@@ -51,6 +58,29 @@ fn run(mut terminal: DefaultTerminal) -> Result<()> {
     Ok(())
 }
 
-fn render(frame: &mut Frame) {
-    Paragraph::new("Hello, Ratatui!").render(frame.area(), frame.buffer_mut());
+fn render(frame: &mut Frame, app_state: &AppState) {
+    // app_state in this case is reborrowed by render function
+    // as it has become an immutable reference from a mutable reference in the run function.
+    // This allows us to read from app_state without modifying it.
+
+    let [border_area] = Layout::vertical([Constraint::Fill(1)])
+        .margin(1)
+        .areas(frame.area());
+
+    let [inner_area] = Layout::vertical([Constraint::Fill(1)])
+        .margin(1)
+        .areas(border_area);
+
+    Block::bordered()
+        .border_type(BorderType::Rounded)
+        .fg(Color::Yellow)
+        .render(border_area, frame.buffer_mut());
+
+    List::new(
+        app_state
+            .items
+            .iter()
+            .map(|x| ListItem::from(x.description.clone())),
+    )
+    .render(inner_area, frame.buffer_mut());
 }
